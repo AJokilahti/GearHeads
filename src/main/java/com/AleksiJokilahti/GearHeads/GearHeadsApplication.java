640b5056334bcf96a4cb9f244853e9832abf5b7a
AJokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-03-31 13:04:32 +0300

    Added project files and dependencies Spring web tools Spring Data JPA H2 database Spring security Thymeleaf

---
+++ src/main/java/com/AleksiJokilahti/GearHeads/GearHeadsApplication.java
@@ -0,0 +1,13 @@
+package com.AleksiJokilahti.GearHeads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearHeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearHeadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AleksiJokilahti/GearHeads/GearHeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AleksiJokilahti.GearHeads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearHeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
