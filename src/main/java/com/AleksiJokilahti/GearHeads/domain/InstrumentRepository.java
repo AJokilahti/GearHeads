c6b1ae8e7a5d2cc0179dc7a745e2d9684bd4717b
AJokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-04-07 13:06:58 +0300

    Added repositories for Instrument and Category Imported them to GearController

---
+++ src/main/java/com/AleksiJokilahti/GearHeads/domain/InstrumentRepository.java
@@ -0,0 +1,8 @@
+package com.AleksiJokilahti.GearHeads.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InstrumentRepository extends CrudRepository<Instrument, Long>{
+
+	
+}
--- src/main/java/com/AleksiJokilahti/GearHeads/web/GearController.java
+++ src/main/java/com/AleksiJokilahti/GearHeads/web/GearController.java
@@ -1,15 +1,28 @@
 package com.AleksiJokilahti.GearHeads.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.AleksiJokilahti.GearHeads.domain.CategoryRepository;
+import com.AleksiJokilahti.GearHeads.domain.InstrumentRepository;
+
 @Controller
 public class GearController {
 
-	@RequestMapping(value = "/gear", method = RequestMethod.GET)
-	public String Instruments(Model model) {
-		return "gear";
-	}
+	@Autowired
+	private InstrumentRepository instrumentrepository;
+	
+	@Autowired
+	private CategoryRepository categoryrepository;
+	
+		@RequestMapping(value = "/gear", method = RequestMethod.GET)
+		public String Instruments(Model model) {
+			model.addAttribute("gear", instrumentrepository);
+			return "gear";
+		}
+		
+		
 }
